f1a71e0a4859b42e257b8292380292dbc2e315ea
sirwilliamiv <wjconnolly2@gmail.com>
2016-12-05 11:21:53 -0600

    added json files and wrote out basic plan

---
+++ seasonal-sales.js
@@ -0,0 +1,56 @@
+// Your job is to
+// build a web page that lists all of the products, the name of the department it's
+//in, and the price.
+//
+//
+//function listProductsInDepartment {
+    //'<h1>${name of department}</h1>
+    //<ul>
+    //  <li>${product name} + ${price}</li>
+    //</ul>
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// Additionally, put a <select> element at the top of the page
+//  that contains all possible values of the season_discount key in the categories file.
+//  how to display these items????
+// // <select name="select">
+//   <option value="value1"></option>
+//   <option value="value2" selected>Value 2</option>
+//   <option value="value3">Value 3</option>
+//   </select>
+// /
+// / As soon as you select one of the seasons,
+// // all prices on the page should immediately be discounted by
+// // the corresponding percentage.
+// function switchPrice () {
+//  switch the prices
+// }
+
+// // For example,
+// //  when Spring is chosen,
+// //  all products in the corresponding Household category should
+// //  have their prices updated with a 15% discount off the base price.
+
+// // The two JSON representations
+// //  above should be in two files: products.json, and categories.json.
+// //  You should load both file via XHRs and store the contents in
+// //  two different JavaScript variables in your code.
